fed5ef3f0e0103106d47940c39f1fbf5cc28a958
crownytrex <152218129+crownytrex@users.noreply.github.com>
2025-09-11 14:17:43 -0400

    Create Syollenoid controller1.py

---
+++ Syollenoid controller1.py
@@ -0,0 +1,11 @@
+# written by crownytrex on github
+# hi
+# 
+import motors
+import os
+import raspian
+import py
+import clock
+import arduino
+print("i have a question, can you do this?")
+if button press = message.1 then print("message.1 Working")
